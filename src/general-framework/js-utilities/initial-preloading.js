8b26e6fc2228c2bec032bce1e172a59a1a16d9b5
Maciej Sawicki <saw.maciej@gmail.com>
2018-05-04 14:50:33 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/initial-preloading.js
+++ src/general-framework/js-utilities/initial-preloading.js
@@ -3,7 +3,7 @@
 - subpages are preloaded on page load into their containers
 - Only one active subpage is shown at a time
 - Tabs and subpages containers share the same id and are binded together this way
-*/
+*///
 
 //preload all views into respective containers
 //until the subpages are loaded the UI is covered by loading overlay
