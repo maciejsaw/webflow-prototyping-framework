1e1d92993b069ca84dd052766fb58ce9c9114130
Maciej Sawicki <saw.maciej@gmail.com>
2018-05-04 11:20:03 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/initial-preloading.js
+++ src/general-framework/js-utilities/initial-preloading.js
@@ -121,7 +121,7 @@ function recursivelyPreloadElements() {
 	};
 
 	var checkIfEverythingIsPreloaded = function() {
-		if ( $('[preload-element-from]').not('[preloading-started]').not('[preloading-done]').lenght > 0) {
+		if ( $('[preload-element-from]').not('[preloading-started]').not('[preloading-done]').length > 0) {
 			return true;
 		} else {
 			return false;
