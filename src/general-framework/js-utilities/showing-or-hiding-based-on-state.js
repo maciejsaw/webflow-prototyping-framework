9aa021cd5535db701faf103772285fba57ed8cfa
Maciej Sawicki <saw.maciej@gmail.com>
2018-06-14 10:30:07 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
+++ src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
@@ -13,6 +13,14 @@ function ReactiveLocalStorageDependVisibilityOnParam(paramName) {
 	ReactiveLocalStorage.onParamChange(paramName, function(value) {
 		$('[depends-on-param="'+paramName+'"]').not('[action-show-when-param-equals="'+value+'"]').not('[action-hide-when-param-equals]').addClass('is-hidden');
 		$('[depends-on-param="'+paramName+'"]').filter('[action-show-when-param-equals="'+value+'"]').removeClass('is-hidden');
+		$('[depends-on-param="'+paramName+'"]').filter('[action-show-when-param-not-equals]').not('[action-show-when-param-equals="'+value+'"]').not('[action-hide-when-param-equals]').each(function() {
+			var paramToCompare = $(this).attr('action-show-when-param-not-equals');
+			if (paramToCompare !== value) {
+				$(this).addClass('is-hidden');
+			} else {
+				$(this).removeClass('is-hidden');
+			}
+		});
 	});
 }
 
