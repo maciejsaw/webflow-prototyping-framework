0e9739eaaf29c51748f1fbcc5f7ca4038cb6d651
Maciej Sawicki <saw.maciej@gmail.com>
2018-06-14 11:05:58 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
+++ src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
@@ -12,7 +12,7 @@ function ReactiveLocalStorageOnParamChangeShowElementsOnlyWhenParamXEqualsY(para
 function ReactiveLocalStorageDependVisibilityOnParam(paramName) {
 	ReactiveLocalStorage.onParamChange(paramName, function(value) {
 		//TODOrefactor needed
-		$('[depends-on-param="'+paramName+'"]').not('[action-show-when-param-equals="'+value+'"]').not('[action-hide-when-param-equals]').not('[action-hide-when-param-not-equals]').addClass('is-hidden');
+		$('[depends-on-param="'+paramName+'"]').not('[action-show-when-param-equals="'+value+'"]').not('[action-hide-when-param-equals]').not('[action-show-when-param-not-equals]').addClass('is-hidden');
 		$('[depends-on-param="'+paramName+'"]').filter('[action-show-when-param-equals="'+value+'"]').removeClass('is-hidden');
 		$('[depends-on-param="'+paramName+'"]').filter('[action-show-when-param-not-equals]').each(function() {
 			var paramToCompare = $(this).attr('action-show-when-param-not-equals');
@@ -28,7 +28,7 @@ function ReactiveLocalStorageDependVisibilityOnParam(paramName) {
 
 function ReactiveLocalStorageHideWhenParamEquals(paramName) {
 	ReactiveLocalStorage.onParamChange(paramName, function(value) {
-		$('[depends-on-param="'+paramName+'"]').not('[action-hide-when-param-equals="'+value+'"]').not('[action-show-when-param-equals]').not('[action-hide-when-param-not-equals]').removeClass('is-hidden');
+		$('[depends-on-param="'+paramName+'"]').not('[action-hide-when-param-equals="'+value+'"]').not('[action-show-when-param-equals]').not('[action-show-when-param-not-equals]').removeClass('is-hidden');
 		$('[depends-on-param="'+paramName+'"]').filter('[action-hide-when-param-equals="'+value+'"]').addClass('is-hidden');
 	});
 }
