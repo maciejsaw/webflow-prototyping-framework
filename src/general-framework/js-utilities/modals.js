bcc7447a09a74e41a0bcef4b5989a8eca8a63a7a
Maciej Sawicki <saw.maciej@gmail.com>
2018-06-19 16:01:15 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/modals.js
+++ src/general-framework/js-utilities/modals.js
@@ -8,8 +8,8 @@ $(document).on('click', '[action-open-modal]', function(event) {
 });
 
 $(document).on('click', '[action-close-modal="true"]', function(event) {
-    QueryStringRouter.removeParam('modalContent', {doNotCreateHistoryState: true});
     QueryStringRouter.goBackBeforeModal();
+    QueryStringRouter.removeParam('modalContent', {doNotCreateHistoryState: true});
 });
 
 function bindEscButtonToCloseModal() {
