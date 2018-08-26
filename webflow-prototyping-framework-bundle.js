de698778df8d644afd13fe298037520d543043ad
Maciej Sawicki <>
2018-08-26 19:19:35 +0200

    automatic commit from webflow-prototyping-framework

--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -2741,7 +2741,7 @@ ReactiveLocalStorage.setDefaultParamAndValidationRules = function(param, options
 	if (options && options.default) {
 		ReactiveLocalStorage.setDefaultParam(param, options.default);
 	}
-	ReactiveLocalStorage.registerParamValidator(param, options.validationFunction(value));
+	ReactiveLocalStorage.registerParamValidator(param, options.validationFunction);
 }
 
 // validation on blur for elements with additional attribute validate-on-blur
