28132e0c16471a380dfbae54eb13b2fb766bdb0e
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 10:46:35 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
+++ src/general-framework/js-utilities/showing-or-hiding-based-on-state.js
@@ -82,3 +82,53 @@ $(document).on('preloadingComplete', function() {
 		}
 	});
 });
+
+function ReactiveLocalStorageShowByExpression() {
+	ReactiveLocalStorage.onParamChange(paramName, function(value) {
+		var bindedItems = $('[data-param="'+paramName+'"]');
+		bindedItems.each(function() {
+			var $this = $(this);
+			var param = $this.attr('data-param');
+			var expression = $this.attr('visible-when');
+			if (!!expression) {
+				$this.removeClass('is-hidden');
+			}
+		});
+	});
+}
+
+//helpful shortcuts for showing and hiding with addClass and removeClass
+
+(function( $ ) {
+  $.fn.isHidden = function(customClass) {
+
+  	customClass = customClass || 'is-hidden';
+
+    this.each(function() {
+      $(this).addClass(customClass);
+    });
+
+    return this;
+  };
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isShown = function(customClass) {
+
+  	customClass = customClass || 'is-hidden';
+
+    this.each(function() {
+      $(this).removeClass(customClass);
+    });
+
+    return this;
+  };
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isNotShown = $.fn.isHidden;
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isNotHidden = $.fn.isShown;
+}( jQuery ));
