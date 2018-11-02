3e744fb883573fd60b4d27e07c21ed480b397727
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 10:47:20 +0100

    automatic commit from webflow-prototyping-framework

---
+++ src/general-framework/js-utilities/semantic-shortcuts.js
@@ -0,0 +1,36 @@
+
+//helpful shortcuts for showing and hiding with addClass and removeClass
+
+(function( $ ) {
+  $.fn.isHidden = function(customClass) {
+
+  	customClass = customClass || 'is-hidden';
+
+    this.each(function() {
+      $(this).addClass(customClass);
+    });
+
+    return this;
+  };
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isShown = function(customClass) {
+
+  	customClass = customClass || 'is-hidden';
+
+    this.each(function() {
+      $(this).removeClass(customClass);
+    });
+
+    return this;
+  };
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isNotShown = $.fn.isHidden;
+}( jQuery ));
+
+(function( $ ) {
+  $.fn.isNotHidden = $.fn.isShown;
+}( jQuery ));
