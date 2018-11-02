090d5112b95469af8ce7b6f9835ae8700f71bfcf
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 18:08:42 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/debug-overlay.js
+++ src/general-framework/js-utilities/debug-overlay.js
@@ -26,3 +26,16 @@ $(document).on('keydown', function(e) {
 
 	}
 });
+
+//https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -4280,4 +4280,17 @@ $(document).on('keydown', function(e) {
 	}
 });
 
+//https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
 //# sourceMappingURL=webflow-prototyping-framework-bundle.js.map
