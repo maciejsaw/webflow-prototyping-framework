52951bfd6a7bea490a78d4fbb3a501fd6a73df14
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 18:13:31 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/debug-overlay.js
+++ src/general-framework/js-utilities/debug-overlay.js
@@ -1,6 +1,5 @@
 function webflowPrototypingFrameworkShowDebugOverlay() {
 
-
 	var allParams = ReactiveLocalStorage.getAllParams();
 
 	const allParamsOrdered = {};
@@ -10,7 +9,7 @@ function webflowPrototypingFrameworkShowDebugOverlay() {
 
 	var overlay = $('<div js-debug-overlay style="position:fixed; left: 30px; bottom: 0; padding: 10px; max-height: 300px; max-width: 600px; background: #fff; overflow: auto; z-index: 1000; box-shadow: 10px 10px 108px 0px rgba(0,0,0,0.75);"></div>');
 
-	$.each(allParams, function(key, value) {
+	$.each(allParamsOrdered, function(key, value) {
 		var row = $('<div><span style="padding-right: 8px;">'+key+'</span><input value="'+value+'" action-text-input="'+key+'"></input></div>');
 		row.appendTo(overlay);
 	});
