15eac3531072f9436610fe7f0b491f90c070a80f
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 10:19:50 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/tabs.js
+++ src/general-framework/js-utilities/tabs.js
@@ -1,24 +1,28 @@
 $(document).on('preloadingComplete', function() {
 
+	var bindedTabParams = {};
+
 	$('[action-tab]').each(function() {
 		var tabsGroup = $(this).attr('tabs-group');
 		var tabIdToBind = $(this).attr('action-tab');
 
-		QueryStringRouter.onParamChange('tabs__'+tabsGroup, function(value) {
-			if (typeof value != 'undefined') {
-				$('[tabs-group="'+tabsGroup+'"][tab-id="'+value+'"]').removeClass('is-hidden');
-				$('[tabs-group="'+tabsGroup+'"]').not('[tab-id="'+value+'"]').addClass('is-hidden');
+		if (!bindedTabParams[tabsGroup]) {
+			QueryStringRouter.onParamChange('tabs__'+tabsGroup, function(value) {
+				if (typeof value != 'undefined') {
+					$('[tabs-group="'+tabsGroup+'"][tab-id="'+value+'"]').removeClass('is-hidden');
+					$('[tabs-group="'+tabsGroup+'"][tab-id]').not('[tab-id="'+value+'"]').addClass('is-hidden');
 
-				$('[tabs-group="'+tabsGroup+'"][action-tab]').not('[action-tab="'+value+'"]').removeClass('is-current');
-				$('[tabs-group="'+tabsGroup+'"][action-tab="'+value+'"]').addClass('is-current');
-			}
-		});
-
-	}); 
+					$('[tabs-group="'+tabsGroup+'"][action-tab]').not('[action-tab="'+value+'"]').removeClass('is-current');
+					$('[tabs-group="'+tabsGroup+'"][action-tab="'+value+'"]').addClass('is-current');
+				}
+			});
+			bindedTabParams[tabsGroup] = true;
+		}
+	});
 
 	$(document).on('click', '[action-tab]', function(event) {
 		var clickedTab = $(this).attr('action-tab');
 		var clickedTabGroup = $(this).attr('tabs-group');
-		QueryStringRouter.setParam('tabs__'+clickedTabGroup, clickedTab);
+		QueryStringRouter.setParam('tabs__'+clickedTabGroup, clickedTab, {doNotCreateHistoryState: true});
 	});
 });
