e080bca5f99f1c4119ff357efd31d5ca68d4092e
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 18:04:08 +0100

    automatic commit from webflow-prototyping-framework

--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -4254,7 +4254,7 @@ On.touchstartOrClick = function(selector, eventHandlerFunction) {
 function webflowPrototypingFrameworkShowDebugOverlay() {
 	var allParams = ReactiveLocalStorage.getAllParams();
 
-	var overlay = $('<div style="position:fixed; left: 30px; bottom: 0; padding: 10px; max-height: 300px; max-width: 600px; background: #fff; overflow: auto; z-index: 1000; box-shadow: 10px 10px 108px 0px rgba(0,0,0,0.75);"></div>');
+	var overlay = $('<div js-debug-overlay style="position:fixed; left: 30px; bottom: 0; padding: 10px; max-height: 300px; max-width: 600px; background: #fff; overflow: auto; z-index: 1000; box-shadow: 10px 10px 108px 0px rgba(0,0,0,0.75);"></div>');
 
 	$.each(allParams, function(key, value) {
 		var row = $('<div><span style="padding-right: 8px;">'+key+'</span><input value="'+value+'" action-text-input="'+key+'"></input></div>');
@@ -4264,10 +4264,19 @@ function webflowPrototypingFrameworkShowDebugOverlay() {
 	overlay.appendTo('body');
 }
 
+function webflowPrototypingFrameworkHideDebugOverlay() {
+	$('[js-debug-overlay]').remove();
+}
+
 $(document).on('keydown', function(e) {
 	console.log('keydown');
 	if ( e.shiftKey && ( e.which === 192 ) ) {
-	  webflowPrototypingFrameworkShowDebugOverlay();
+	 	if ($('[js-debug-overlay]').length === 0) {
+	 		webflowPrototypingFrameworkShowDebugOverlay();
+	 	} else {
+	 		webflowPrototypingFrameworkHideDebugOverlay();
+	 	}
+
 	}
 });
 
