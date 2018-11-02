3b3a5c8a671b09b84018b949a0c23e31ce42165e
Maciej Sawicki <saw.maciej@gmail.com>
2018-11-02 17:53:12 +0100

    automatic commit from webflow-prototyping-framework

--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -4251,4 +4251,23 @@ On.touchstartOrClick = function(selector, eventHandlerFunction) {
 	});
 };
 
+function webflowPrototypingFrameworkShowDebugOverlay() {
+	var allParams = ReactiveLocalStorage.getAllParams();
+
+	var overlay = $('<div style="position:fixed; left: 30px; bottom: 0; padding: 10px; max-height: 300px; max-width: 600px; background: #fff; overflow: auto; z-index: 1000; box-shadow: 10px 10px 108px 0px rgba(0,0,0,0.75);"></div>');
+
+	$.each(allParams, function(key, value) {
+		var row = $('<div><span style="padding-right: 8px;">'+key+'</span><input value="'+value+'" action-text-input="'+key+'"></input></div>');
+		row.appendTo(overlay);
+	});
+
+	overlay.appendTo('body');
+}
+
+$(document).on('keypress', function(e) {
+	if ( e.ctrlKey && ( e.which === 46 ) ) {
+	  webflowPrototypingFrameworkShowDebugOverlay();
+	}
+});
+
 //# sourceMappingURL=webflow-prototyping-framework-bundle.js.map
