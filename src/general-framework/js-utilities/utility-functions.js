9eb0e518be393116d64d5df9dc42aac2575603a0
Maciej Sawicki <saw.maciej@gmail.com>
2019-04-12 18:16:08 +0200

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/utility-functions.js
+++ src/general-framework/js-utilities/utility-functions.js
@@ -89,6 +89,7 @@ function isNotEmpty(val) {
 }
 
 //https://stackoverflow.com/questions/6921275/is-it-possible-to-chain-settimeout-functions-in-javascript
+debugger;
 function delay(t, fn) {
     // private instance variables
     var queue = [], self, timer;
--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -547,6 +547,44 @@ function isNotEmpty(val) {
   return !isEmpty(val);
 }
 
+//https://stackoverflow.com/questions/6921275/is-it-possible-to-chain-settimeout-functions-in-javascript
+debugger;
+function delay(t, fn) {
+    // private instance variables
+    var queue = [], self, timer;
+
+    function schedule(t, fn) {
+        timer = setTimeout(function() {
+            timer = null;
+            fn();
+
+            if (queue.length) {
+                var item = queue.shift();
+                schedule(item.t, item.fn);
+            }
+        }, t);
+    }
+    self = {
+        delay: function(t, fn) {
+            // if already queuing things or running a timer,
+            //   then just add to the queue
+            if (queue.length || timer) {
+                queue.push({fn: fn, t: t});
+            } else {
+                // no queue or timer yet, so schedule the timer
+                schedule(t, fn);
+            }
+            return self;
+        },
+        cancel: function() {
+            clearTimeout(timer);
+            queue = [];
+            return self;
+        }
+    };
+    return self.delay(t, fn);
+}
+
 //fix common typos
 function fixCommonAttrTypos() {
   $('[action-shown-when-param-equals]').each(function() {
