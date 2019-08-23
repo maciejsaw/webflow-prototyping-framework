9e3df11344dc52015735f0e884bb83b866c27027
Maciej Sawicki <saw.maciej@gmail.com>
2019-08-23 17:35:27 +0200

    automatic commit from webflow-prototyping-framework

--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -3976,7 +3976,7 @@ $(document).on('preloadingComplete', function() { //need to wait for all the aja
 
 });
 
-$(document).on('click', '[action-text-input][allow-only-numbers]', function(event) {
+$(document).on('click', '[action-text-input][allow-numbers-only]', function(event) {
 
     // Old browsers fallback
     if (!event || !event.key) {
