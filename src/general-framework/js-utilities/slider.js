23fa59e85de94dd2a15020a975544519ced4ab65
Maciej Sawicki <saw.maciej@gmail.com>
2019-11-08 14:04:21 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/slider.js
+++ src/general-framework/js-utilities/slider.js
@@ -119,7 +119,7 @@ function initSlidersDragging() {
 
     $swipableArea.on('click', function(event) {
       var clickedXCoordinate = event.pageX - $swipableArea.offset.left;
-      var sliderWidth = $swipableArea.outerWidth;
+      var sliderWidth = $swipableArea.outerWidth();
       console.log(clickedXCoordinate);
       var percetangeThatWasSet = clickedXCoordinate / sliderWidth;
       var resultNumber = (maxValue - minValue)*percetangeThatWasSet + Number(minValue);
--- webflow-prototyping-framework-bundle.js
+++ webflow-prototyping-framework-bundle.js
@@ -4826,7 +4826,7 @@ function initSlidersDragging() {
 
     $swipableArea.on('click', function(event) {
       var clickedXCoordinate = event.pageX - $swipableArea.offset.left;
-      var sliderWidth = $swipableArea.outerWidth;
+      var sliderWidth = $swipableArea.outerWidth();
       console.log(clickedXCoordinate);
       var percetangeThatWasSet = clickedXCoordinate / sliderWidth;
       var resultNumber = (maxValue - minValue)*percetangeThatWasSet + Number(minValue);
