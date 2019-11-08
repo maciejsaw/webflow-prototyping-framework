10a94674861faf5d697d538d4550880c8ca41c60
Maciej Sawicki <saw.maciej@gmail.com>
2019-11-08 15:53:12 +0100

    automatic commit from webflow-prototyping-framework

--- src/general-framework/js-utilities/slider.js
+++ src/general-framework/js-utilities/slider.js
@@ -120,7 +120,7 @@ function initSlidersDragging() {
     $swipableArea.on('click', function(event) {
       console.log(event);
       var eventVieportPageX = event.pageX - window.scrollX;
-      var areaCoordinates = $swipableArea[0].getBoundingClientRect;
+      var areaCoordinates = $swipableArea[0].getBoundingClientRect();
       var clickedPercentage = (eventVieportPageX - areaCoordinates.left) / (areaCoordinates.right - areaCoordinates.left);
       var resultNumber = (maxValue - minValue)*clickedPercentage + Number(minValue);
       var resultNumberRounded = roundToClosestMultipleOf(interval, resultNumber);
